88784fe0cc684b516a9c59a2c9921334f19bb8ae
alex-31g <al.tsimbalyuk@gmail.com>
2021-04-23 22:51:26 +0300

    add

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,3 +1,16 @@
 const console = require('./_console.log');
 // ==========================================
 
+// Сортировка слиянием / Merge sort
+// Быстрая сортировка / Quick sort / qsort
+// Бинарный поиск / Binary search
+// Поиск в глубину / Depth-first search / DFS
+// Поиск в ширину / Breadth-first search / BFS
+// Алгоритм Дейкстры / Dijkstra algorithm
+
+// ==========================================
+
+// Рекурсия
+// https://learn.javascript.ru/recursion
+// https://github.com/utimur/algs_and_structures_course
+// https://www.youtube.com/watch?v=NErrGZ64OdE&t=321s
